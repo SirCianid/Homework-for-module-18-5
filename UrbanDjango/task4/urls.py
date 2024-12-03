fd0e8b60ef54df16b534aef2faf8a5456319832d
Павел Сотченков <dr.sot4enkoff2017@yandex.ru>
2024-12-03 20:17:40 +0500

    Delete UrbanDjango directory

--- UrbanDjango/task4/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('main_pg/', main_pg_view),
-    path('shop_pg/', games_shp_page),
-    path('cart_pg/', cart_page),
-]
--- UrbanDjango/task5/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('django_sign_up/', sign_up_by_django),
-    path('html_sign_up/', sign_up_by_html),
-]
